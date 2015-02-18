fdd62a529dcf75c59f5cbc33b1363f6c4577fc61
stefand12 <stefand12@ru.is>
2015-02-18 21:05:15 +0000

    smá html og css bætt við

--- src/js/RoomController.js
+++ src/js/RoomController.js
@@ -1,4 +1,4 @@
-angular.module("NewIrc").controller("RoomController", function ($scope) {
+angular.module("NewIrc").controller("RoomController", function ($scope, $location, $rootScope, $routeParams, socket) {
 	$scope.currentUser = $routeParams.user;
 	$scope.currentChannel = $routeParams.room;
 	$scope.message = "Hello from room";
