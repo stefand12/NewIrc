e906640556a77bdfb6dd5079371e72d9c7f4b1a7
Her Gunnar <gunnarkp13@ru.is>
2015-02-18 19:29:20 +0000

    roomlist farinn ad loada og meira til

--- src/js/RoomController.js
+++ src/js/RoomController.js
@@ -1,3 +1,5 @@
 angular.module("NewIrc").controller("RoomController", function ($scope) {
+	$scope.currentUser = $routeParams.user;
+	$scope.currentChannel = $routeParams.room;
 	$scope.message = "Hello from room";
 })
--- src/js/RoomListController.js
+++ src/js/RoomListController.js
@@ -1,5 +1,14 @@
 angular.module("NewIrc").controller("RoomListController", function ($scope, $location, $rootScope, $routeParams, socket) {
 	//TODO GET ROOM LIST
-	$scope.rooms = ['Room 1','Room 2','Room 3','Room 4','Room 5'];
-	$scope.currentUser = $routeParams.user;
+	//
+		$scope.currentUser = $routeParams.user;
+		console.log("choo");
+		socket.emit('rooms');
+		socket.on('roomlist', function (data) {
+			var test = [];
+			for(var x in data){console.log(x); test.push(x);}; 
+			$scope.rooms = test;
+		});
+	//$scope.rooms = ['Room 1','Room 2','Room 3','Room 4','Room 5'];
+	
 })
