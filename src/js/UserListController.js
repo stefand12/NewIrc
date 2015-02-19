c4342d05fb903582383f749f52122e8c218a899c
Guðjón Pétursson <gudjon12@ru.is>
2015-02-19 00:24:36 +0000

    sma i vidbot userlisst

--- src/js/UserListController.js
+++ src/js/UserListController.js
@@ -1,6 +1,6 @@
 angular.module("NewIrc").controller("UserListController", function ($scope, $location, $rootScope, $routeParams, socket) {
 	
-	$scope. = $routeParams.users;
+	$scope.currentUser = $routeParams.users;
 		console.log("tippauser");
 		socket.emit('users');
 		socket.on('userlist', function (data) {
