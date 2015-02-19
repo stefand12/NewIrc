def6d8102dba678fe6e2d49c8f6b2ccc74872e1a
Her Gunnar <gunnarkp13@ru.is>
2015-02-19 16:07:50 +0000

    commit smá breyt og laga head

--- src/js/ChatController.js
+++ src/js/ChatController.js
@@ -1,3 +1,15 @@
-angular.module("NewIrc").controller("ChatController", function ($scope) {
+angular.module("NewIrc").controller("ChatController", function ($scope, $location, $rootScope, $routeParams, socket) {
 	$scope.message = "Hello from chat";
+	$scope.messageText = "";
+	$scope.currentUser = $routeParams.users;
+	$scope.currentRoom = $routeParams.room;
+
+	$scope.login = function() {
+		var messageObj = {
+				nick : socket.username,
+				timestamp :  new Date(),
+				message : messageText.substring(0, 200)
+			};
+			rooms[$scope.currentRoom].addMessage(messageObj);
+	};
 })
