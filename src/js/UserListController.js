7132ace6824d35505cfafcafd01e168bcca148eb
Guðjón Pétursson <gudjon12@ru.is>
2015-02-19 00:19:33 +0000

    sma laga userlisthtml

--- src/js/UserListController.js
+++ src/js/UserListController.js
@@ -1,22 +1,15 @@
 angular.module("NewIrc").controller("UserListController", function ($scope, $location, $rootScope, $routeParams, socket) {
 	
-	$scope.allusers = $routeParams.users;
+	$scope. = $routeParams.users;
 		console.log("tippauser");
 		socket.emit('users');
 		socket.on('userlist', function (data) {
 			var listtest = [];
-
-
-			//console.log(Object.keys(data));
 			for(var x in data){
-
-			//console.log(Object.keys(x));
 			listtest.push(data[x]);
-			//console.log(data[x]);
-		}; 
+			}; 
 			$scope.users = listtest;
 
+});
 
-		});
-	$scope.message = "Hello from userlist";
 })
