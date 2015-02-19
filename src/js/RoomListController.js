264d41a67c913d77cf957b957302598581c6c469
stefand12 <stefand12@ru.is>
2015-02-19 00:35:10 +0000

    new room function added

--- src/js/RoomListController.js
+++ src/js/RoomListController.js
@@ -9,6 +9,51 @@ angular.module("NewIrc").controller("RoomListController", function ($scope, $loc
 			for(var x in data){console.log(x); test.push(x);}; 
 			$scope.rooms = test;
 		});
+
+		$scope.roomName = '';
+		$scope.errorMessage = '';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		$scope.createRoom = function() {
+			var newRoom = {
+				room: $scope.roomName,
+				pass: undefined
+			};
+
+			if($scope.roomName === '') {
+				console.log("rassgataPíka");
+				$scope.errorMessage = "Choose a name for your channel";
+			}
+			else {
+				console.log("píka");
+				socket.emit('joinroom', newRoom, function (succsess, reason) {
+					if(!succsess){
+						$scope.errorMessage = reason;
+					}
+					else
+						{console.log("mamma");}
+				});
+			}
+		};
+		/*socket.on('joinroom', function(a,true){
+			console.log("creating new room");
+		})*/
 	//$scope.rooms = ['Room 1','Room 2','Room 3','Room 4','Room 5'];
+
+
+
 	
 })
