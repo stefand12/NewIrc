6e7ac80c347e139c4719c18dcb9d77cbdcd1fcaf
Her Gunnar <gunnarkp13@ru.is>
2015-02-21 17:06:38 +0000

    jshint lint free á password promtpt

--- src/js/RoomController.js
+++ src/js/RoomController.js
@@ -1,3 +1,100 @@
+/*jslint browser:true */
+
+/*
+JavaScript Password Prompt by Luc (luc@ltdinteractive.com)
+Originaly posted to http://stackoverflow.com/questions/9554987/how-can-i-hide-the-password-entered-via-a-javascript-dialog-prompt
+This code is Public Domain :)
+
+Syntax:
+password_prompt(label_message, button_message, callback);
+password_prompt(label_message, button_message, width, height, callback);
+
+Example usage:
+password_prompt("Please enter your password:", "Submit", function(password) {
+    alert("Your password is: " + password);
+});
+*/
+
+var password_prompt = function (label_message, button_message, arg3, arg4, arg5) {
+	var callback;
+	var width;
+	var height;
+    if (typeof label_message !== "string") { 
+    	label_message = "Password:"; 
+    }
+    if (typeof button_message !== "string") { 
+    	button_message = "Submit"; 
+    }
+    if (typeof arg3 === "function") {
+        callback = arg3;
+    }
+    else if (typeof arg3 === "number" && typeof arg4 === "number" && typeof arg5 === "function") {
+        width = arg3;
+        height = arg4;
+        callback = arg5;
+    }
+    if (typeof width !== "number") {
+    	width = 200;
+    }
+    if (typeof height !== "number") {
+    	height = 100;
+    }
+    if (typeof callback !== "function") {
+    	callback = function (password) {};
+    }
+
+    var submit = function () {
+        callback(input.value);
+        document.body.removeChild(div);
+        window.removeEventListener("resize", resize, false);
+    };
+    var resize = function () {
+        div.style.left = ((window.innerWidth / 2) - (width / 2)) + "px";
+        div.style.top = ((window.innerHeight / 2) - (height / 2)) + "px";
+    };
+
+    var div = document.createElement("div");
+    div.id = "password_prompt";
+    div.style.background = "white";
+    div.style.color = "black";
+    div.style.border = "1px solid black";
+    div.style.width = width + "px";
+    div.style.height = height + "px";
+    div.style.padding = "16px";
+    div.style.position = "fixed";
+    div.style.left = ((window.innerWidth / 2) - (width / 2)) + "px";
+    div.style.top = ((window.innerHeight / 2) - (height / 2)) + "px";
+
+    var label = document.createElement("label");
+    label.id = "password_prompt_label";
+    label.innerHTML = label_message;
+    label.for = "password_prompt_input";
+    div.appendChild(label);
+
+    div.appendChild(document.createElement("br"));
+
+    var input = document.createElement("input");
+    input.id = "password_prompt_input";
+    input.type = "password";
+    input.addEventListener("keyup", function (e) {
+        if (e.keyCode == 13) { 
+        	submit();
+        }
+    }, false);
+    div.appendChild(input);
+
+    div.appendChild(document.createElement("br"));
+    div.appendChild(document.createElement("br"));
+
+    var button = document.createElement("button");
+    button.innerHTML = button_message;
+    button.addEventListener("click", submit, false);
+    div.appendChild(button);
+
+    document.body.appendChild(div);
+    window.addEventListener("resize", resize, false);
+};
+
 angular.module("NewIrc").controller("RoomController", function ($scope, $location, $rootScope, $routeParams, socket, sharedVariables) {
 	$scope.currentUser = $routeParams.user;
 	$scope.currentRoom = $routeParams.room;
@@ -24,13 +121,14 @@ angular.module("NewIrc").controller("RoomController", function ($scope, $locatio
 	socket.emit('joinroom', test, function (success, reason) {
 		console.log("joinroom emitted with room name: " + $routeParams.room);
 	if (!success) {
-		if(reason === "wrong password"){
+		if(reason === "wrong password") {
 
 			password_prompt("Please enter your password:", "Submit", function (passWrd) {
-				$rootScope.$apply(function (){
+				$rootScope.$apply(function () {
 					$location.path('/room/' + $scope.currentUser +'/'+ $scope.currentRoom +'/'+ passWrd);
 				});
 			});
+
 
 			
 			//console.log("hér er password " + password);
@@ -140,7 +238,7 @@ angular.module("NewIrc").controller("RoomController", function ($scope, $locatio
 		});
 	};
 
-	$scope.leaveRoom = function() {
+	$scope.leaveRoom = function () {
 		socket.emit("partroom", $scope.currentRoom);
 		$location.path('/rooms/' + $scope.currentUser);
 	};
@@ -149,68 +247,4 @@ angular.module("NewIrc").controller("RoomController", function ($scope, $locatio
 });
 
 
-window.password_prompt = function(label_message, button_message, arg3, arg4, arg5) {
 
-    if (typeof label_message !== "string") var label_message = "Password:";
-    if (typeof button_message !== "string") var button_message = "Submit";
-    if (typeof arg3 === "function") {
-        var callback = arg3;
-    }
-    else if (typeof arg3 === "number" && typeof arg4 === "number" && typeof arg5 === "function") {
-        var width = arg3;
-        var height = arg4;
-        var callback = arg5;
-    }
-    if (typeof width !== "number") var width = 200;
-    if (typeof height !== "number") var height = 100;
-    if (typeof callback !== "function") var callback = function(password){};
-
-    var submit = function() {
-        callback(input.value);
-        document.body.removeChild(div);
-        window.removeEventListener("resize", resize, false);
-    };
-    var resize = function() {
-        div.style.left = ((window.innerWidth / 2) - (width / 2)) + "px";
-        div.style.top = ((window.innerHeight / 2) - (height / 2)) + "px";
-    };
-
-    var div = document.createElement("div");
-    div.id = "password_prompt";
-    div.style.background = "white";
-    div.style.color = "black";
-    div.style.border = "1px solid black";
-    div.style.width = width + "px";
-    div.style.height = height + "px";
-    div.style.padding = "16px";
-    div.style.position = "fixed";
-    div.style.left = ((window.innerWidth / 2) - (width / 2)) + "px";
-    div.style.top = ((window.innerHeight / 2) - (height / 2)) + "px";
-
-    var label = document.createElement("label");
-    label.id = "password_prompt_label";
-    label.innerHTML = label_message;
-    label.for = "password_prompt_input";
-    div.appendChild(label);
-
-    div.appendChild(document.createElement("br"));
-
-    var input = document.createElement("input");
-    input.id = "password_prompt_input";
-    input.type = "password";
-    input.addEventListener("keyup", function(e) {
-        if (e.keyCode == 13) submit();
-    }, false);
-    div.appendChild(input);
-
-    div.appendChild(document.createElement("br"));
-    div.appendChild(document.createElement("br"));
-
-    var button = document.createElement("button");
-    button.innerHTML = button_message;
-    button.addEventListener("click", submit, false);
-    div.appendChild(button);
-
-    document.body.appendChild(div);
-    window.addEventListener("resize", resize, false);
-};
