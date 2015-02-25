16d03592fd5a7148b1aef4f2f40c2b9440ec0983
GunniKalli <gunnarkp13@ru.is>
2015-02-25 16:46:40 +0000

    login controller snyrtur og commentaður

--- src/js/LoginController.js
+++ src/js/LoginController.js
@@ -1,26 +1,41 @@
+/* LoginController */
+/*
+	LoginController sér um að logga user in.
+	tekur inn socket og sharedVariables til að 
+	uppfæra notenda fyrir aðra controllera
+*/
+
 angular.module("NewIrc").controller("LoginController", [
 	'$scope',
-	 '$location',
-	  '$rootScope',
-	   '$routeParams',
-	    'socket',
-	     'sharedVariables',
-	      'privateMessage',
-	       function ($scope, $location, $rootScope, $routeParams, socket, sharedVariables) {
-	$scope.errorMessage = '';
-	$scope.nickname = '';
-	$scope.login = function() {
-		if ($scope.nickname === '') {
-			$scope.errorMessage = 'Please choose a nick-name before continuing!';
-		} else {
-			socket.emit('adduser', $scope.nickname, function (available) {
-				if (available) {
-					sharedVariables.setUser($scope.nickname);
-					$location.path('/rooms/' + $scope.nickname);
-				} else {
-					$scope.errorMessage = 'This nick-name is unavailable!';
-				}
-			});			
-		}
-	};
-}]);
+	'$location',
+	'$rootScope',
+	'$routeParams',
+	'socket',
+	'sharedVariables',
+	'privateMessage',
+	function ($scope, $location, $rootScope, $routeParams, socket, sharedVariables) {
+		$scope.errorMessage = '';
+		$scope.nickname = '';
+
+		/*
+			Function sem sér um að logga þig inn
+			emittar adduser eventi á server sem 
+			síðan svara hvort það hafi tekist eða ekki.
+			redirectar eftir því á roomcontroller eða ekki.
+		*/
+		$scope.login = function() {
+			if ($scope.nickname === '') {
+				$scope.errorMessage = 'Please choose a nick-name before continuing!';
+			} else {
+				socket.emit('adduser', $scope.nickname, function (available) {
+					if (available) {
+						sharedVariables.setUser($scope.nickname);
+						$location.path('/rooms/' + $scope.nickname);
+					} else {
+						$scope.errorMessage = 'This nick-name is unavailable!';
+					}
+				});			
+			}
+		};
+	}
+]);
