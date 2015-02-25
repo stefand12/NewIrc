f7a65dd01e061a154808bd320b757f838534bdd0
GunniKalli <gunnarkp13@ru.is>
2015-02-25 11:54:55 +0000

    bootstrap komið inn í NewIrc.min.js, lagaði version af boostrap í bower.json og commentaði út línurnar í index.html þangað til er búið að prófa á fleiri tölvum

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,19 @@ module.exports = function ( grunt ) {
   			dist: {
     			// the files to concatenate
     			src: [
+    				'bower_components/bootstrap/js/affix.js',
+    				'bower_components/bootstrap/js/alert.js',
+    				'bower_components/bootstrap/js/button.js',
+    				'bower_components/bootstrap/js/carousel.js',
+    				'bower_components/bootstrap/js/collapse.js',
+    				'bower_components/bootstrap/js/dropdown.js',
+    				'bower_components/bootstrap/js/modal.js',
+    				'bower_components/bootstrap/js/tooltip.js',
+    				'bower_components/bootstrap/js/popover.js',
+    				'bower_components/bootstrap/js/scrollspy.js',
+    				'bower_components/bootstrap/js/tab.js',   				
+    				'bower_components/bootstrap/js/transition.js',
+    				'bower_components/bootstrap/js/bootstrap.js',
     				'bower_components/underscore/underscore.js',
     				'bower_components/angular/angular.js',
     				'bower_components/angular-route/angular-route.js',
