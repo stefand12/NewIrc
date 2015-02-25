ab6b8d61aa96090d8e9d3952420324df72388859
GunniKalli <gunnarkp13@ru.is>
2015-02-25 16:26:31 +0000

    about og chatController commented og snyrtir

--- src/js/AboutController.js
+++ src/js/AboutController.js
@@ -1,6 +1,14 @@
+/* AboutController */
+/*
+	controller sem er að mestu ónotaður í annað en að 
+	injecta ng-view, hugsunin var
+	að hafa controller ef við myndum vilja gera eitthvað 
+	meira við viewið
+*/
 angular.module("NewIrc").controller("AboutController", [
 	'$scope',
 	 function ($scope) {
-	$scope.message = "About NewIrc";
-}]);
+		$scope.message = "About NewIrc";
+	}
+]);
 
--- src/js/ChatController.js
+++ src/js/ChatController.js
@@ -1,27 +1,37 @@
+/* ChatController */
+/*
+	Controller sem sér um að senda skilaboð inn á rásir á server 
+	sem eru síðan updated af RoomController, tekur inn socket og
+	sharedVariables services, til að geta talað á milli controllera
+*/
 angular.module("NewIrc").controller("ChatController", [
 	'$scope',
-	 '$location',
-	  '$rootScope',
-	   '$routeParams',
-	    'socket',
-	     'sharedVariables',
-	       function ($scope, $location, $rootScope, $routeParams, socket, sharedVariables) {
-	$scope.message = "You are logged in as:";
-	$scope.msgText = '';
-	$scope.currentUser = sharedVariables.getUser();
-	$scope.currentRoom = sharedVariables.getRoom();
+	'$location',
+	'$rootScope',
+	'$routeParams',
+	'socket',
+	'sharedVariables',
+	function ($scope, $location, $rootScope, $routeParams, socket, sharedVariables) {
+		$scope.message = "You are logged in as:";
+		$scope.msgText = '';
+		$scope.currentUser = sharedVariables.getUser();
+		$scope.currentRoom = sharedVariables.getRoom();
 
-	$scope.sendMessage = function() {
-		var messageObj = {
-				roomName : $scope.currentRoom,
-				msg : $scope.msgText
+		/*
+			function sem hlustar 'eftir roomlist' event
+			frá server og uppfærir rásalistann.
+		*/
+		$scope.sendMessage = function() {
+			var messageObj = {
+					roomName : $scope.currentRoom,
+					msg : $scope.msgText
 			};
-		socket.emit("sendmsg", messageObj);
-		socket.on('updatechat', function (roomName, msgHis) {
-			if(roomName === $scope.currentRoom){
-				$scope.msgText = "";
-			}
-		});
-	};
-}]);
+			
+			/* emitted events */
+			socket.emit("sendmsg", messageObj);
+			
+			$scope.msgText = "";
+		};
+	}
+]);
 
